6deafd2078238d5e9df373c76f13d91513fc6e3d
partyka <partyka95@icloud.com>
2022-05-18 23:41:36 +0200

    fix: specification

--- src/lib/http-contract.ts
+++ src/lib/http-contract.ts
@@ -1,7 +1,7 @@
-import { RoutePath, HttpRouter, ApiSpec } from '@caviajs/http-router';
+import { RoutePath, HttpRouter, Specification } from '@caviajs/http-router';
 
 export class HttpContract {
   public static setup(path: RoutePath, httpRouter: HttpRouter): void {
-    httpRouter.route({ handler: (): ApiSpec => httpRouter.apiSpec, method: 'GET', path: path });
+    httpRouter.route({ handler: (): Specification => httpRouter.specification, method: 'GET', path: path });
   }
 }
