1abb6552d265c992850a019cfd1d0cc5d1e342dd
Paweł Partyka <partyka95@icloud.com>
2022-06-20 17:28:43 +0200

    test: get schema nullable

---
+++ test/get-schema-nullable.spec.ts
@@ -0,0 +1,10 @@
+import { getSchemaNullable } from '../src/get-schema-nullable';
+
+it('should return false as default value', () => {
+  expect(getSchemaNullable({})).toEqual(false);
+});
+
+it('should return value from property', () => {
+  expect(getSchemaNullable({ nullable: false })).toEqual(false);
+  expect(getSchemaNullable({ nullable: true })).toEqual(true);
+});
