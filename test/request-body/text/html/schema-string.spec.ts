2b6ca7af23b1df1c242ddeb586b69e2980bb60ad
Paweł Partyka <partyka95@icloud.com>
2022-06-23 17:42:26 +0200

    test: request body text/html

---
+++ test/request-body/text/html/schema-string.spec.ts
@@ -0,0 +1,42 @@
+import { HttpRouter } from '@caviajs/http-router';
+import http from 'http';
+import supertest from 'supertest';
+import { HttpContract } from '../../../../src';
+
+it('should convert request stream to string', async () => {
+  let body: any;
+
+  const httpRouter: HttpRouter = new HttpRouter();
+
+  httpRouter
+    .intercept(HttpContract.setup())
+    .route({
+      handler: (request) => {
+        body = request.body;
+      },
+      metadata: {
+        contract: {
+          request: {
+            body: {
+              contentSchema: { type: 'string' },
+              contentType: 'text/html',
+            },
+          }
+        }
+      },
+      method: 'POST',
+      path: '/',
+    });
+
+  const httpServer: http.Server = http.createServer((request, response) => {
+    httpRouter.handle(request, response);
+  });
+
+  await supertest(httpServer)
+    .post('/')
+    .set('Content-Type', 'text/html')
+    .send('Hello World');
+
+  expect(typeof body).toEqual('string');
+  expect(body).toEqual('Hello World');
+});
