ea82c3926acfcd8e1f16563285c41e1856e397b3
Paweł Partyka <partyka95@icloud.com>
2022-06-23 18:38:21 +0200

    test: request body application/json

---
+++ test/request-body/application/json/schema-number.spec.ts
@@ -0,0 +1,42 @@
+import { HttpRouter } from '@caviajs/http-router';
+import http from 'http';
+import supertest from 'supertest';
+import { HttpContract } from '../../../../src';
+
+it('should convert request stream to number', async () => {
+  let body: any;
+
+  const httpRouter: HttpRouter = new HttpRouter();
+
+  httpRouter
+    .intercept(HttpContract.setup())
+    .route({
+      handler: (request) => {
+        body = request.body;
+      },
+      metadata: {
+        contract: {
+          request: {
+            body: {
+              contentSchema: { type: 'number' },
+              contentType: 'application/json',
+            },
+          }
+        }
+      },
+      method: 'POST',
+      path: '/',
+    });
+
+  const httpServer: http.Server = http.createServer((request, response) => {
+    httpRouter.handle(request, response);
+  });
+
+  await supertest(httpServer)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send('1245');
+
+  expect(typeof body).toEqual('number');
+  expect(body).toEqual(1245);
+});
---
+++ test/request-body/application/json/schema-stream.spec.ts
@@ -0,0 +1,49 @@
+import { HttpRouter } from '@caviajs/http-router';
+import http from 'http';
+import supertest from 'supertest';
+import { Readable } from 'stream';
+import { HttpContract } from '../../../../src';
+
+it('should convert request stream to Readable stream', async () => {
+  let body: Readable;
+  let bodyBuffer: Buffer = Buffer.alloc(0);
+
+  const httpRouter: HttpRouter = new HttpRouter();
+
+  httpRouter
+    .intercept(HttpContract.setup())
+    .route({
+      handler: async (request) => {
+        body = request.body;
+
+        await new Promise<void>(resolve => {
+          request.body.on('data', (chunk: Buffer) => bodyBuffer = Buffer.concat([bodyBuffer, chunk]));
+          request.body.on('end', () => resolve());
+        });
+      },
+      metadata: {
+        contract: {
+          request: {
+            body: {
+              contentSchema: { type: 'stream' },
+              contentType: 'application/json',
+            },
+          }
+        }
+      },
+      method: 'POST',
+      path: '/',
+    });
+
+  const httpServer: http.Server = http.createServer((request, response) => {
+    httpRouter.handle(request, response);
+  });
+
+  await supertest(httpServer)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(JSON.stringify({ foo: 'cavia', bar: 1245 }));
+
+  expect(body instanceof Readable).toEqual(true);
+  expect(JSON.parse(bodyBuffer.toString())).toEqual({ foo: 'cavia', bar: 1245 });
+});
