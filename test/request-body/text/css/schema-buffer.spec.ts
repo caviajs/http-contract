72d08d2c63f5d1e3dbdc9c8e4de64194a72fd91c
Paweł Partyka <partyka95@icloud.com>
2022-06-23 17:42:12 +0200

    test: request body text/css

---
+++ test/request-body/text/css/schema-buffer.spec.ts
@@ -0,0 +1,42 @@
+import { HttpRouter } from '@caviajs/http-router';
+import http from 'http';
+import supertest from 'supertest';
+import { HttpContract } from '../../../../src';
+
+it('should convert request stream to Buffer', async () => {
+  let body: any;
+
+  const httpRouter: HttpRouter = new HttpRouter();
+
+  httpRouter
+    .intercept(HttpContract.setup())
+    .route({
+      handler: (request) => {
+        body = request.body;
+      },
+      metadata: {
+        contract: {
+          request: {
+            body: {
+              contentSchema: { type: 'buffer' },
+              contentType: 'text/css',
+            },
+          }
+        }
+      },
+      method: 'POST',
+      path: '/',
+    });
+
+  const httpServer: http.Server = http.createServer((request, response) => {
+    httpRouter.handle(request, response);
+  });
+
+  await supertest(httpServer)
+    .post('/')
+    .set('Content-Type', 'text/css')
+    .send('Hello World');
+
+  expect(Buffer.isBuffer(body)).toEqual(true);
+  expect(body.toString()).toEqual('Hello World');
+});
