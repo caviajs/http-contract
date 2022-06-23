8ad49c1f1c744b53a451cdd56ef6138ed31176c3
Paweł Partyka <partyka95@icloud.com>
2022-06-23 18:45:07 +0200

    test: request body application/json

---
+++ test/request-body/application/json/schema-object.spec.ts
@@ -0,0 +1,42 @@
+import { HttpRouter } from '@caviajs/http-router';
+import http from 'http';
+import supertest from 'supertest';
+import { HttpContract } from '../../../../src';
+
+it('should convert request stream to object', async () => {
+  let body: any;
+
+  const httpRouter: HttpRouter = new HttpRouter();
+
+  httpRouter
+    .intercept(HttpContract.setup())
+    .route({
+      handler: (request) => {
+        body = request.body;
+      },
+      metadata: {
+        contract: {
+          request: {
+            body: {
+              contentSchema: { type: 'object' },
+              contentType: 'application/json',
+            },
+          }
+        }
+      },
+      method: 'POST',
+      path: '/',
+    });
+
+  const httpServer: http.Server = http.createServer((request, response) => {
+    httpRouter.handle(request, response);
+  });
+
+  await supertest(httpServer)
+    .post('/')
+    .set('Content-Type', 'application/json')
+    .send(JSON.stringify({ foo: 'cavia', bar: 1245 }));
+
+  expect(typeof body).toEqual('object');
+  expect(body).toEqual({ foo: 'cavia', bar: 1245 });
+});
