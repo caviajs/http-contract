189a373a192cc9091b49d5efe4b72b39dc992086
Paweł Partyka <partyka95@icloud.com>
2022-06-25 17:03:24 +0200

    test: request headers

--- test/request-headers/schema-enum.spec.ts
+++ test/request-headers/schema-enum.spec.ts
@@ -1,149 +1,94 @@
-import { HttpRouter, RouteMetadata } from '@caviajs/http-router';
+import { HttpRouter } from '@caviajs/http-router';
 import http from 'http';
 import supertest from 'supertest';
-import { HttpContract } from '../../src';
+import { HttpContract, SchemaEnum, ValidationError } from '../../src';
+import * as schemaEnum from '../../src/schema-enum';
 
-function createServer(routeMetadata: RouteMetadata): http.Server {
-  const httpRouter: HttpRouter = new HttpRouter();
+const HEADER_NAME: string = 'x-cavia';
+const HEADER_VALUE: string = 'hello';
+const HEADER_SCHEMA: SchemaEnum = { enum: ['hello', 'world'], type: 'enum' };
+const PATH: string[] = ['request', 'headers', HEADER_NAME];
 
-  httpRouter
-    .intercept(HttpContract.setup())
-    .route({
-      handler: () => undefined,
-      metadata: routeMetadata,
-      method: 'POST',
-      path: '/',
-    });
-
-  return http.createServer((request, response) => {
-    httpRouter.handle(request, response);
-  });
-}
-
-it('should validate the enum condition correctly', async () => {
-  const httpServer = createServer({
-    contract: {
-      request: {
-        headers: {
-          'x-cavia': {
-            enum: ['Hello', 'World'],
-            nullable: false,
-            required: true,
-            type: 'enum',
-          },
-        },
-      },
-    },
+describe('SchemaEnum', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  // valid
-  {
+  it('should attempt to convert the data to string and then call validateSchemaEnum', async () => {
+    const validateSchemaEnumSpy = jest.spyOn(schemaEnum, 'validateSchemaEnum');
+
+    let header: any;
+
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .intercept(HttpContract.setup())
+      .route({
+        handler: (request) => {
+          header = request.headers[HEADER_NAME];
+        },
+        metadata: {
+          contract: {
+            request: {
+              headers: {
+                [HEADER_NAME]: HEADER_SCHEMA,
+              },
+            },
+          },
+        },
+        method: 'POST',
+        path: '/',
+      });
+
+    const httpServer: http.Server = http.createServer((request, response) => {
+      httpRouter.handle(request, response);
+    });
+
+    await supertest(httpServer)
+      .post('/')
+      .set(HEADER_NAME, HEADER_VALUE);
+
+    expect(typeof header).toEqual('string');
+    expect(header).toEqual(HEADER_VALUE);
+
+    expect(validateSchemaEnumSpy).toHaveBeenNthCalledWith(1, HEADER_SCHEMA, HEADER_VALUE, PATH);
+  });
+
+  it('should return 400 if validateSchemaEnum return an array with errors', async () => {
+    const errors: ValidationError[] = [{ message: 'Lorem ipsum', path: PATH.join('.') }];
+
+    jest
+      .spyOn(schemaEnum, 'validateSchemaEnum')
+      .mockImplementation(() => errors);
+
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .intercept(HttpContract.setup())
+      .route({
+        handler: () => undefined,
+        metadata: {
+          contract: {
+            request: {
+              headers: {
+                [HEADER_NAME]: HEADER_SCHEMA,
+              },
+            },
+          },
+        },
+        method: 'POST',
+        path: '/',
+      });
+
+    const httpServer: http.Server = http.createServer((request, response) => {
+      httpRouter.handle(request, response);
+    });
+
     const response = await supertest(httpServer)
       .post('/')
-      .set('X-Cavia', 'Hello');
+      .set(HEADER_NAME, HEADER_VALUE);
 
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // valid
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'World');
-
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // invalid
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'Foo');
-
-    expect(response.body).toEqual([
-      { message: 'The value must be one of the following values: Hello, World', path: 'request.headers.x-cavia' },
-    ]);
-    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    expect(response.body).toEqual(errors);
     expect(response.statusCode).toEqual(400);
-  }
-});
-
-it('should validate the required condition correctly', async () => {
-  // required: false (default)
-  {
-    const httpServer = createServer({
-      contract: {
-        request: {
-          headers: {
-            'x-cavia': {
-              enum: ['Hello', 'World'],
-              type: 'enum',
-            },
-          },
-        },
-      },
-    });
-
-    const response = await supertest(httpServer)
-      .post('/');
-
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // required: false
-  {
-    const httpServer = createServer({
-      contract: {
-        request: {
-          headers: {
-            'x-cavia': {
-              enum: ['Hello', 'World'],
-              required: false,
-              type: 'enum',
-            },
-          },
-        },
-      },
-    });
-
-    const response = await supertest(httpServer)
-      .post('/');
-
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // required: true
-  {
-    const httpServer = createServer({
-      contract: {
-        request: {
-          headers: {
-            'x-cavia': {
-              enum: ['Hello', 'World'],
-              required: true,
-              type: 'enum',
-            },
-          },
-        },
-      },
-    });
-
-    const response = await supertest(httpServer)
-      .post('/');
-
-    expect(response.body).toEqual([
-      { message: 'The value is required', path: 'request.headers.x-cavia' },
-      { message: 'The value must be one of the following values: Hello, World', path: 'request.headers.x-cavia' },
-    ]);
-    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-    expect(response.statusCode).toEqual(400);
-  }
+  });
 });
--- test/request-headers/schema-string.spec.ts
+++ test/request-headers/schema-string.spec.ts
@@ -1,250 +1,94 @@
-import { HttpRouter, RouteMetadata } from '@caviajs/http-router';
+import { HttpRouter } from '@caviajs/http-router';
 import http from 'http';
 import supertest from 'supertest';
-import { HttpContract } from '../../src';
+import { HttpContract, SchemaString, ValidationError } from '../../src';
+import * as schemaString from '../../src/schema-string';
 
-function createServer(routeMetadata: RouteMetadata): http.Server {
-  const httpRouter: HttpRouter = new HttpRouter();
+const HEADER_NAME: string = 'x-cavia';
+const HEADER_VALUE: string = 'hello';
+const HEADER_SCHEMA: SchemaString = { type: 'string' };
+const PATH: string[] = ['request', 'headers', HEADER_NAME];
 
-  httpRouter
-    .intercept(HttpContract.setup())
-    .route({
-      handler: () => undefined,
-      metadata: routeMetadata,
-      method: 'POST',
-      path: '/',
-    });
-
-  return http.createServer((request, response) => {
-    httpRouter.handle(request, response);
+describe('SchemaString', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
   });
-}
 
-it('should validate the expressions condition correctly', async () => {
-  const httpServer = createServer({
-    contract: {
-      request: {
-        headers: {
-          'x-cavia': {
-            expressions: [
-              /^[A-Z]/,
-              /[A-Z]$/,
-            ],
-            type: 'string',
-          },
+  it('should attempt to convert the data to string and then call validateSchemaString', async () => {
+    const validateSchemaStringSpy = jest.spyOn(schemaString, 'validateSchemaString');
+
+    let header: any;
+
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .intercept(HttpContract.setup())
+      .route({
+        handler: (request) => {
+          header = request.headers[HEADER_NAME];
         },
-      },
-    },
-  });
-
-  // valid
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'FoO');
-
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // invalid
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'Foo');
-
-    expect(response.body).toEqual([
-      { message: 'The value should match a regular expression /[A-Z]$/', path: 'request.headers.x-cavia' },
-    ]);
-    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-    expect(response.statusCode).toEqual(400);
-  }
-
-  // invalid
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'foo');
-
-    expect(response.body).toEqual([
-      { message: 'The value should match a regular expression /^[A-Z]/', path: 'request.headers.x-cavia' },
-      { message: 'The value should match a regular expression /[A-Z]$/', path: 'request.headers.x-cavia' },
-    ]);
-    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-    expect(response.statusCode).toEqual(400);
-  }
-});
-
-it('should validate the maxLength condition correctly', async () => {
-  const httpServer = createServer({
-    contract: {
-      request: {
-        headers: {
-          'x-cavia': {
-            maxLength: 10,
-            type: 'string',
-          },
-        },
-      },
-    },
-  });
-
-  // longer than maxLength
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'HelloHelloHello');
-
-    expect(response.body).toEqual([
-      { message: 'The value must be shorter than or equal to 10 characters', path: 'request.headers.x-cavia' },
-    ]);
-    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-    expect(response.statusCode).toEqual(400);
-  }
-
-  // equal to maxLength
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'HelloHello');
-
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // shorter than maxLength
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'Hello');
-
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-});
-
-it('should validate the minLength condition correctly', async () => {
-  const httpServer = createServer({
-    contract: {
-      request: {
-        headers: {
-          'x-cavia': {
-            minLength: 10,
-            type: 'string',
-          },
-        },
-      },
-    },
-  });
-
-  // longer than minLength
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'HelloHelloHello');
-
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // equal to minLength
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'HelloHello');
-
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // shorter than minLength
-  {
-    const response = await supertest(httpServer)
-      .post('/')
-      .set('X-Cavia', 'Hello');
-
-    expect(response.body).toEqual([
-      { message: 'The value must be longer than or equal to 10 characters', path: 'request.headers.x-cavia' },
-    ]);
-    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-    expect(response.statusCode).toEqual(400);
-  }
-});
-
-it('should validate the required condition correctly', async () => {
-  // required: false (default)
-  {
-    const httpServer = createServer({
-      contract: {
-        request: {
-          headers: {
-            'x-cavia': {
-              type: 'string',
+        metadata: {
+          contract: {
+            request: {
+              headers: {
+                [HEADER_NAME]: HEADER_SCHEMA,
+              },
             },
           },
         },
-      },
+        method: 'POST',
+        path: '/',
+      });
+
+    const httpServer: http.Server = http.createServer((request, response) => {
+      httpRouter.handle(request, response);
     });
 
-    const response = await supertest(httpServer)
-      .post('/');
+    await supertest(httpServer)
+      .post('/')
+      .set(HEADER_NAME, HEADER_VALUE);
 
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
+    expect(typeof header).toEqual('string');
+    expect(header).toEqual(HEADER_VALUE);
 
-  // required: false
-  {
-    const httpServer = createServer({
-      contract: {
-        request: {
-          headers: {
-            'x-cavia': {
-              required: false,
-              type: 'string',
+    expect(validateSchemaStringSpy).toHaveBeenNthCalledWith(1, HEADER_SCHEMA, HEADER_VALUE, PATH);
+  });
+
+  it('should return 400 if validateSchemaString return an array with errors', async () => {
+    const errors: ValidationError[] = [{ message: 'Lorem ipsum', path: PATH.join('.') }];
+
+    jest
+      .spyOn(schemaString, 'validateSchemaString')
+      .mockImplementation(() => errors);
+
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .intercept(HttpContract.setup())
+      .route({
+        handler: () => undefined,
+        metadata: {
+          contract: {
+            request: {
+              headers: {
+                [HEADER_NAME]: HEADER_SCHEMA,
+              },
             },
           },
         },
-      },
+        method: 'POST',
+        path: '/',
+      });
+
+    const httpServer: http.Server = http.createServer((request, response) => {
+      httpRouter.handle(request, response);
     });
 
     const response = await supertest(httpServer)
-      .post('/');
+      .post('/')
+      .set(HEADER_NAME, HEADER_VALUE);
 
-    expect(response.body).toEqual({});
-    expect(response.headers['content-type']).toBeUndefined();
-    expect(response.statusCode).toEqual(200);
-  }
-
-  // required: true
-  {
-    const httpServer = createServer({
-      contract: {
-        request: {
-          headers: {
-            'x-cavia': {
-              required: true,
-              type: 'string',
-            },
-          },
-        },
-      },
-    });
-
-    const response = await supertest(httpServer)
-      .post('/');
-
-    expect(response.body).toEqual([
-      { message: 'The value is required', path: 'request.headers.x-cavia' },
-      { message: 'The value should be string', path: 'request.headers.x-cavia' },
-    ]);
-    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    expect(response.body).toEqual(errors);
     expect(response.statusCode).toEqual(400);
-  }
+  });
 });
