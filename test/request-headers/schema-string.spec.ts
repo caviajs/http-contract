1f4ca2fb2e495dbd3cdd88dae86da7ec7e6718e0
Paweł Partyka <partyka95@icloud.com>
2022-06-27 17:59:11 +0200

    test: request headers string

--- test/request-headers/schema-string.spec.ts
+++ test/request-headers/schema-string.spec.ts
@@ -54,6 +54,41 @@ describe('SchemaString', () => {
     expect(validateSchemaStringSpy).toHaveBeenNthCalledWith(1, HEADER_SCHEMA, HEADER_VALUE, PATH);
   });
 
+  it('header declared in the schema but not passed by the client should not exist in the request.headers object', async () => {
+    let headerExists: boolean;
+
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .intercept(HttpContract.setup())
+      .route({
+        handler: (request) => {
+          headerExists = Object.keys(request.headers).includes(HEADER_NAME);
+        },
+        metadata: {
+          contract: {
+            request: {
+              headers: {
+                // declared in schema
+                [HEADER_NAME]: HEADER_SCHEMA,
+              },
+            },
+          },
+        },
+        method: 'POST',
+        path: '/',
+      });
+
+    const httpServer: http.Server = http.createServer((request, response) => {
+      httpRouter.handle(request, response);
+    });
+
+    await supertest(httpServer)
+      .post(`/`); // but not passed by the client
+
+    expect(headerExists).toBeFalsy();
+  });
+
   it('should return 400 if validateSchemaString return an array with errors', async () => {
     const errors: ValidationError[] = [{ message: 'Lorem ipsum', path: PATH.join('.') }];
 
